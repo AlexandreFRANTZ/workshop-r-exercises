80519b6b754c16f2be81b1a4e5a1077b86af9142
Frantz <alexandre.frantz@epitech.eu>
2023-01-27 15:55:12 +0100

    Exercise ex06

---
+++ exercises/ex06.R
@@ -0,0 +1,19 @@
+# Now you've learned to use the data, you need to learn to read and write data !
+# Open and read the "csvTestFile.csv" file to render the a graph of it, replace every value from the "Series_title_3" into the number 42 and rewrite it in a "csvResult.csv" file
+# Here are the steps:
+# Set correctly your env to locate the file
+
+# Your code
+
+# Read and store the file content
+
+# Your code
+plot(csvFile$Period, csvFile$Data_value)
+
+# Replace the values from the "Series_title_3" column
+
+# Your code
+
+# Create a new file and write inside of it
+
+# Your code
